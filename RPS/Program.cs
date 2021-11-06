28c5e4f35f1564cc98c19dcbbbdbb16595a315f9
KatyaDremliuh <nerona.91@gmail.com>
2021-11-06 15:56:16 +0300

    Propose to start the game.

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -8,8 +8,27 @@ namespace RPS
         {
             Console.Write("Input Ur name, please: ");
             string playersName = Console.ReadLine();
-            Console.WriteLine("Hi! {0}", playersName);
+            Console.WriteLine("Hi, {0}!", playersName);
+
+            GetAnswerIfPlayerIsReadyOrNot(playersName);
+
             Console.ReadKey();
+        }
+
+        private static void GetAnswerIfPlayerIsReadyOrNot( string playersName)
+        {
+            string readyToPlay = "YES";
+
+            Console.WriteLine("Let's play a game \"Rock-Paper-Scissors\". \nIf U're ready, write {0}", readyToPlay);
+
+            if (readyToPlay.Equals(Console.ReadLine(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("Hurrah!"); 
+            }
+            else
+            {
+                Console.WriteLine($"Okay, {playersName} we'll play another time. Goodbye!");
+            }
         }
     }
 }
